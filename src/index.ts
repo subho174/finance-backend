bab66752280be68c68aaf06a19d15f103b681ea1
Subhodip <subhodipnebu51@gmail.com>
2026-04-06 12:08:48 +0530

    fix: Add trust proxy for rate limiting

--- src/index.ts
+++ src/index.ts
@@ -38,6 +38,8 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+app.set("trust proxy", 1);
+
 app.get("/", (_, res: Response) => {
   res.send("Server is running !");
 });
@@ -76,6 +78,7 @@ app.use(
   },
 );
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
